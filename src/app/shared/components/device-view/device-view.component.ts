5794655908e9b2c08a939a6847454e77268c1bc8
hamza.rehman <hamza.rehman@cloudsolutions.com.sa>
2026-05-08 16:19:19 +0300

    device view component added

---
+++ src/app/shared/components/device-view/device-view.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-device-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './device-view.component.html',
+  styleUrl: './device-view.component.css',
+})
+export class DeviceViewComponent {
+}
