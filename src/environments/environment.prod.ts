2202ec2f1d56815152969378f7ccebee5c687672
hamza.rehman <hamza.rehman@cloudsolutions.com.sa>
2026-06-22 10:47:31 +0300

    env updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://localhost:7170/api',
+  // baseUrl: 'https://localhost:7170/api',
+  baseUrl: 'https://hamzarehman033-001-site1.jtempurl.com/api',
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:7170/api',
+  // baseUrl: 'https://localhost:7170/api',
+  baseUrl: 'https://hamzarehman033-001-site1.jtempurl.com/api',
+  
 };
