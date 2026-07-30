35dd60af6d247bd3dab1412c3dbfae39d601b506
hamza.rehman <hamza.rehman@cloudsolutions.com.sa>
2026-07-30 20:28:56 +0300

    env updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   // baseUrl: 'https://localhost:7170/api',
   // signalrHubUrl: 'http://localhost:7170/hubs/device-data',
-  baseUrl: 'https://iotdashboard-cua2ehdzbwgnefba.eastasia-01.azurewebsites.net/api',
-  signalrHubUrl: 'https://iotdashboard-cua2ehdzbwgnefba.eastasia-01.azurewebsites.net/hubs/device-data',
-
+  // baseUrl: 'https://iotdashboard-cua2ehdzbwgnefba.eastasia-01.azurewebsites.net/api',
+  // signalrHubUrl: 'https://iotdashboard-cua2ehdzbwgnefba.eastasia-01.azurewebsites.net/hubs/device-data',
+  baseUrl: 'https://iotdashboard-qa-c0b8audvfzbkhfd0.eastasia-01.azurewebsites.net/api',
+  signalrHubUrl: 'https://iotdashboard-qa-c0b8audvfzbkhfd0.eastasia-01.azurewebsites.net/hubs/device-data',
 };
