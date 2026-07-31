326b2d0ea3b9ff427c269964a85eb1cd1fb55451
hamza.rehman <hamza.rehman@cloudsolutions.com.sa>
2026-07-31 13:47:36 +0300

    new  build with dynamic domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
+const domain = window.location.hostname.includes("github") ? 
+'https://iotdashboard-cua2ehdzbwgnefba.eastasia-01.azurewebsites.net' : 
+'https://iotdashboard-qa-c0b8audvfzbkhfd0.eastasia-01.azurewebsites.net';
+
 export const environment = {
   production: true,
   // baseUrl: 'https://localhost:7170/api',
   // signalrHubUrl: 'http://localhost:7170/hubs/device-data',
-  // baseUrl: 'https://iotdashboard-cua2ehdzbwgnefba.eastasia-01.azurewebsites.net/api',
-  // signalrHubUrl: 'https://iotdashboard-cua2ehdzbwgnefba.eastasia-01.azurewebsites.net/hubs/device-data',
-  baseUrl: 'https://iotdashboard-qa-c0b8audvfzbkhfd0.eastasia-01.azurewebsites.net/api',
-  signalrHubUrl: 'https://iotdashboard-qa-c0b8audvfzbkhfd0.eastasia-01.azurewebsites.net/hubs/device-data',
+  baseUrl: domain + '/api',
+  signalrHubUrl: domain + '/hubs/device-data',
 };
