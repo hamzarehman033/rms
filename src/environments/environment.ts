dc2adbb195cc940cd3678d7be3e2a46cdb9c2a9d
hamza.rehman <hamza.rehman@cloudsolutions.com.sa>
2026-08-06 08:59:48 +0300

    ENV fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
-const domain = (window.location.hostname || false) === 'localhost' ? 'https://localhost:7170' : 'https://iotdashboard-cua2ehdzbwgnefba.eastasia-01.azurewebsites.net';
+const domain = window.location.hostname === 'localhost' ? 'https://localhost:7170' : 'https://iotdashboard-cua2ehdzbwgnefba.eastasia-01.azurewebsites.net';
 
 export const environment = {
   production: false,
-  isLocal: false,
-  baseUrl: 'https://iotdashboard-qa-c0b8audvfzbkhfd0.eastasia-01.azurewebsites.net' + '/api',
+  isLocal: true,
+  baseUrl: domain + '/api',
   signalrHubUrl: domain + '/hubs/device-data',
 };
